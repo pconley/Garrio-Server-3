cb7b1b7d3fe8087c879966154d1c1cbd45990ded
Pat Conley <pconley312@gmail.com>
2017-12-27 07:23:38 -0500

    structure page layout

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+
+  def show
+  	@page = params[:page].downcase
+  	template = "pages/#{@page}"
+	exists = lookup_context.template_exists?(template)
+    if exists
+    	render template: template
+    else
+    	redirect_to error_url
+    end
+  end
+
+  def error
+  	@page = 'error'
+  	render template: "pages/error", :locals => {:page => 'error'}
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root 'widgets#index'
+  root "pages#show", page: "home"
+  get "/pages/error", to: 'pages#error', as: 'error'
+  get "/pages/:page" => "pages#show", as: 'pages'
   resources :widgets
 end
