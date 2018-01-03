c1dd88080619b22f2c754fcfa1c37d7813a8cd66
Pat Conley <pconley312@gmail.com>
2018-01-02 21:34:27 -0500

    add host to title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_domain
+
+  def set_domain
+  	@domain = URI.parse(request.original_url).host
+  	# puts "*** #{host}"
+  	# @domain = host.split('.')[-2]
+  end
 end
